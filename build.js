6dfde57425e2966c3847a45efcbad7bf7f659607
Robert Anderson <robert.anderson.fl@gmail.com>
2019-08-25 11:52:00 -0400

    Working auto-generation of the last modified date

--- build.js
+++ build.js
@@ -2,18 +2,48 @@
 
 console.log('js file build');
 
+// ----------------
+// - Dependencies -
+// ----------------
+
 var Metalsmith  = require('metalsmith');
 var in_place    = require('metalsmith-in-place');
 
-Metalsmith(__dirname)         // __dirname defined by node.js:
-                              // name of current working directory
-	.metadata({               // add any variable you want
-                              // use them in layout-files
+// ------------------------
+// - Function Definitions -
+// ------------------------
+
+function getCurrentDate() {
+	var today = new Date();	
+
+	return today.toDateString()
+}
+
+// --------------------
+// - Global variables -
+// --------------------
+
+// Using the convention of 'g_' to indicate a global variable
+
+var g_last_modified = getCurrentDate();
+
+// --------------
+// - Build step -
+// --------------
+
+// __dirname defined by node.js:
+// add any variable you want
+// use them in layout-files
+
+Metalsmith(__dirname)         
+                              
+	.metadata({
     	sitename: "boilerplate site",
     	siteurl: "http://example.com",
     	description: "",
     	generatorname: "Metalsmith",
-    	generatorurl: "http://metalsmith.io/"
+    	generatorurl: "http://metalsmith.io/",
+    	g_last_modified: g_last_modified,
   	})
 	.source('./src')            // source directory
 	.destination('./build')     // destination directory
